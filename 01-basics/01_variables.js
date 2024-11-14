599ea58bc7f86267b05508dc123a430770d913f5
SnehaKudmate <156420751+SnehaKudmate@users.noreply.github.com>
2024-11-14 04:54:28 +0000

    datatypes and variable

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 123433
+let accountEmail = "sn16@gmail.com"
+var accountPassword = "156sn"
+accountCity = "amt "
+let accountState;
+//accountId=2//not allowed
+/*
+prefer not to use var
+because of the issue in block scope and functional scope
+*/
+
+accountEmail="df12@gmail.com"
+accountPassword="70000"
+accountCity="Mumbai"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3)//we are using node js not browser
+
+console.log(3 + 3)
+
+console.log("sneha")
+
+let name= "sneha"
+let age= 18
+let isLoggedIn= false
+let state;
+
+
+//number => 2 to the power 53
+//bigint
+//string = ""
+//boolean => true/false
+//null=> standalone value
+//undefined => 
+//symbol => unique
+
+//object
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+console.log(typeof symbol);
+
+
+
+
+
