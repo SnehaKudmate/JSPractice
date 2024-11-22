ceb69a4a0c1d10edad8e3bd6148d8417857607ca
SnehaKudmate <156420751+SnehaKudmate@users.noreply.github.com>
2024-11-22 09:26:00 +0000

    datatypes done

---
+++ 01-basics/03_datatypeConversion.js
@@ -0,0 +1,28 @@
+//let score= "33abc"//Nan
+//let score=null// it will give 0
+//let score= undefined;//Nan
+//let score = true;//1
+//let score = "sneha";//NaN
+//console.log(typeof(score));
+
+
+//let valueInNumber= Number(score);
+
+//console.log(typeof(valueInNumber));
+//console.log(valueInNumber);
+
+let isLoggedIn= "fg";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1 true
+//0 false
+//"" => false
+//"bhbf"=> true
+
+let someNumber = 33
+let StringNumber = String(someNumber)
+console.log(typeof(StringNumber));
+
+
+
